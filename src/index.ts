18042a9dd6014d019cf25a2a020f31cc3617499b
pauline2003 <paulinemwamaitha68@gmail.com>
2024-06-14 17:07:00 +0300

    change

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,7 @@ app.get('/', (c) => {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kephar's Eatery</title>
+    <title>paline hot dishes</title>
     <style>
         body {
             font-family: Arial, sans-serif;
