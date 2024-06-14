6d7c65da94c78239deb19d36f414e943226cebec
pauline2003 <paulinemwamaitha68@gmail.com>
2024-06-14 16:56:41 +0300

    change

--- src/index.ts
+++ src/index.ts
@@ -52,9 +52,68 @@ app.use(
 );
 
 // default route
-app.get("/success", (c) => {
-  return c.text("The server is working !!");
-});
+app.get('/', (c) => {
+  return c.html(
+    `
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kephar's Eatery</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .container {
+            text-align: center;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            font-size: 2em;
+            margin-bottom: 10px;
+        }
+
+        .emoji {
+            font-size: 1.5em;
+        }
+
+        p {
+            font-size: 1.1em;
+            color: #555;
+        }
+
+        footer {
+            margin-top: 20px;
+            font-size: 0.9em;
+            color: #aaa;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome paulines' hot dishes </h1>
+        <p>From Our Kitchen to Your Heart </p>
+        <p class="emoji">🍟</p>
+        <footer>© 2024 paulines mwaura</footer>
+    </div>
+</body>
+</html>
+
+
+    `)
+})
 
 // my custom  routes
 app.route("/", userRouter); // /users
